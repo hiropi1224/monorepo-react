f5abb5486b558262b4b992fbb93a572315288ffd
hiropi1224 <hiropi1224s@gmail.com>
2024-02-25 16:11:38 +0900

    ♻️

--- apps/demo/pages/index.tsx
+++ apps/demo/pages/index.tsx
@@ -1,35 +1,13 @@
 import { type NextPageWithLayout } from "./_app";
 import { DefaultLayout } from "../components/layout/default-layout";
-import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
 import Link from "next/link";
+import { SampleCard } from "../components/sample-card/sample-card";
 
 const Home: NextPageWithLayout = () => {
   return (
     <main>
       <Link href="/nav">navページ</Link>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Card.Section>
-          <Image
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-            height={160}
-            alt="Norway"
-          />
-        </Card.Section>
-
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>Norway Fjord Adventures</Text>
-          <Badge color="pink">On Sale</Badge>
-        </Group>
-
-        <Text size="sm" c="dimmed">
-          With Fjord Tours you can explore more of the magical fjord landscapes
-          with tours and activities on and around the fjords of Norway
-        </Text>
-
-        <Button color="blue" fullWidth mt="md" radius="md">
-          Book classic tour now
-        </Button>
-      </Card>
+      <SampleCard />
     </main>
   );
 };
