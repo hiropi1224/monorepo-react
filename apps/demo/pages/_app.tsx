31222efb4f09405017d7d269a36b0ec0c74aba9f
hiropi1224 <hiropi1224s@gmail.com>
2024-02-25 14:02:24 +0900

    ➕ add mantine

--- apps/demo/pages/_app.tsx
+++ apps/demo/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { createTheme, MantineProvider } from "@mantine/core";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   // eslint-disable-next-line no-unused-vars
@@ -11,9 +12,17 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
