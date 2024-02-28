9f560e220a4b33c92d1739b865b5fa2767a30240
hiropi1224 <hiropi1224s@gmail.com>
2024-02-28 22:41:06 +0900

    ⬇️

--- apps/work/pages/_app.tsx
+++ apps/work/pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { AppProps } from "next/app";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  // eslint-disable-next-line no-unused-vars
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppPropsWithLayout): JSX.Element {
+  // Use the layout defined at the page level, if available
+  const getLayout =
+    Component.getLayout ||
+    ((page) => {
+      return page;
+    });
+
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
-
-export default MyApp
