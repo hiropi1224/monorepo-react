6b6120d24844d47b84ba09c86e0a55e460d42a34
hiropi1224 <hiropi1224s@gmail.com>
2024-03-15 22:57:47 +0900

    ✨ auth page

---
+++ apps/auth/app/auth/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <nav className="bg-red-500 text-white">This is auth navbar</nav>
+      {children}
+    </div>
+  );
+}
