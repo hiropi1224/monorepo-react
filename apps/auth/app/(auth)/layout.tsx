5ed129eb6b8d04896a9c67499e07736766f67427
hiropi1224 <hiropi1224s@gmail.com>
2024-03-15 23:08:48 +0900

    ✨ Route Groups

--- apps/auth/app/(auth)/layout.tsx
+++ apps/auth/app/(auth)/layout.tsx
@@ -3,7 +3,9 @@ import { ReactNode } from "react";
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <nav className="bg-red-500 text-white">This is auth navbar</nav>
+      <nav className="bg-red-500 text-white">
+        This is auth navbar without /auth prefix
+      </nav>
       {children}
     </div>
   );
