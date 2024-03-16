ca0db941a9b29ba7a533ac984b5f152fe409f1e0
hiropi1224 <hiropi1224s@gmail.com>
2024-03-16 15:14:37 +0900

    💄

--- apps/auth/app/page.tsx
+++ apps/auth/app/page.tsx
@@ -1,11 +1,29 @@
+import { Poppins } from "next/font/google";
+import { LoginButton } from "~/components/auth/login-button";
 import { Button } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: "600",
+});
 
 export default function Home() {
   return (
-    <>
-      <Button size="lg" variant="outline">
-        Click Me
-      </Button>
-    </>
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-r from-blue-800  via-sky-500 to-blue-800 ">
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white", font.className)}>
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ apps/auth/components/auth/login-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ReactNode } from "react";
+
+type LoginButtonProps = {
+  children: ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+};
+
+export function LoginButton({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode === "modal") {
+    return <span>Implement mode</span>;
+  }
+
+  // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+  return <span onClick={onClick}>{children}</span>;
+}
