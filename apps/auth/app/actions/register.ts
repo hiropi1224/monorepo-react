f13c31245e1cc7dbbda8c49075ede631d8586c8f
hiropi1224 <hiropi1224s@gmail.com>
2024-03-16 22:53:39 +0900

    💄 register-form

---
+++ apps/auth/app/actions/register.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { z } from "zod";
+import { RegisterSchema } from "~/schemas";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedField = RegisterSchema.safeParse(values);
+  if (!validatedField.success) {
+    return { error: "Invalid fields" };
+  }
+
+  return { success: "Email sent!" };
+};
