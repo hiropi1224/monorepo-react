2a89c7c30fc03a768d3f2f030c90f61823189435
hiropi1224 <hiropi1224s@gmail.com>
2024-03-17 13:25:38 +0900

    🔧 prisma config

---
+++ apps/auth/lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+// dev環境でのホットリロード
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
